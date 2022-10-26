b95b81293b3c4fc7fdeefb1c8b9d4e602300ed76
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-10-26 14:29:14 -0700

    commented steps to solve JS problem

--- Functions/Problem4/script.js
+++ Functions/Problem4/script.js
@@ -5,3 +5,26 @@ three print "Fizz" instead of the number and for the
   multiples of both three and five print "FizzBuzz".
 
 */
+
+// wrtie a function 
+// declare and initialize counter variable (0)
+
+// declare the number variable to stop count (100)
+
+// loop through every number up to the stop count number (100)
+
+// if the number is a multiple of 3 , print " Fizz"
+
+// if the number is a multiple of 5 print " Buzz"
+
+// if the number is a multiple of 3 and 5 print "FizzBuzz"
+
+// for any other number , print the number
+
+// add 1 to the count after each loop 
+
+function fizzBuzz(){
+
+
+
+}
