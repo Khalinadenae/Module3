ebfe3aa228d59ebad79eecb4dd1a87019e443477
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-10-27 15:06:41 -0700

    created function to solve problem
    Used a for loop to search every character  in the string to see if it is the same as the letter argument

--- Functions/Problem3/script.js
+++ Functions/Problem3/script.js
@@ -9,3 +9,17 @@ two arguments, a string and a letter and the
 //set a variable to count the specified letter occurnaces
 // loop through every character in the string for occurance of the letter 
 // if the letter is found , add 1 to the count variable 
+
+
+function findLetter(str, letter){
+    let letterCount = 0;
+
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === letter ){
+            letterCount = letterCount + 1
+        }
+    }
+    console.log(letterCount)
+}
+
+findLetter("Khalina", "k ")
