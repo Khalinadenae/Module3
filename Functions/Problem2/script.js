59a5099133f207b7fdf31f540825536908195bf2
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-10-27 14:26:30 -0700

    created function and initialized variables to solve problem

--- Functions/Problem2/script.js
+++ Functions/Problem2/script.js
@@ -1,4 +1,13 @@
-/* 
-Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
+//Write a JavaScript function that accepts a string as a parameter and counts the number of vowels within the string.
 
-*/
+
+
+function findVowel(str){
+// initialize variable to count vowels
+let vowelCount = 0;
+// initialize array of vowels for nested array 
+let vowels = ["a", "e", "i", "o", "u"]
+
+
+
+}
