45f8ee027f7b1fbda90e6a34392817b54d8532ca
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-10-27 14:33:39 -0700

    created nested loop
    Loops through the vowel array for every character in the string, checking for a vowel

--- Functions/Problem2/script.js
+++ Functions/Problem2/script.js
@@ -8,6 +8,16 @@ let vowelCount = 0;
 // initialize array of vowels for nested array 
 let vowels = ["a", "e", "i", "o", "u"]
 
-
-
+//loop through the characters in the string
+for(let i = 0; i < str.length; i++){
+    //loop through array of vowels for each character
+    for(let j = 0; j < vowels.length; j++){
+    // check if current index of the string is a vowel
+    if(str[i] === vowels[i]){
+        // if the index of string contains a vowel, increment the vowel counter
+        vowelCount++
+      }
+     }
+   }
+   console.log(vowelCount)
 }
