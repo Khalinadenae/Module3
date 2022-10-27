92acf6881b4574bd3a8184aa5b5468373f4d8966
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-10-27 15:02:45 -0700

    commented steps to solve problem

--- Functions/Problem3/script.js
+++ Functions/Problem3/script.js
@@ -4,3 +4,8 @@ two arguments, a string and a letter and the
  function will count the number of occurrences 
  of the specified letter within the string. 
 */
+
+
+//set a variable to count the specified letter occurnaces
+// loop through every character in the string for occurance of the letter 
+// if the letter is found , add 1 to the count variable 
