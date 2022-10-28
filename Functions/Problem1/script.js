2bf873ef9d1fd742856855b0a505fd9bc9e9e6d9
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-10-28 12:20:37 -0700

    solution for problem 1
    searched the string for the longest word using the string split method and a for loop.

--- Functions/Problem1/script.js
+++ Functions/Problem1/script.js
@@ -1,4 +1,20 @@
 /* 
 Write a JavaScript function that accepts a string as a parameter and find the longest word within the string.
-
 */
+
+function findLongestString(str){
+
+str = str.split(" ")
+
+let longestString = ""
+
+for (let i = 0; i < str.length; i++){
+    if (str[i].length > longestString.length){
+        longestString = str[i]
+    }
+}
+console.log(longestString)
+}
+
+
+findLongestString("Life is a blessing")
