614e9bf8bd4f22c608a5e8d2c86c2d39fbbd5b1e
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-11-01 16:02:16 -0700

    solution problem 3

--- Arrays/Problem3/script.js
+++ Arrays/Problem3/script.js
@@ -2,3 +2,11 @@
 Define a function that takes in an array of numbers/integers and returns the sorted version of that array, greatest to least. (Note: do NOT use the "reverse" method.)
 
 */
+
+function sortBigToSmall(arr){
+    arr = arr.sort((a, b) => b - a );
+      
+      console.log(arr)
+    }
+    
+    sortBigToSmall([2,45,67,89,899,0,6,44,667,4,34,5])
--- Arrays/Problem4/script.js
+++ Arrays/Problem4/script.js
@@ -2,4 +2,19 @@
 Write a JavaScript function to remove duplicate items from an array (consider case sensitivity).
 
 */
-
+  
+  function removeDuplicates(arr){
+    let sorted = []
+    
+    for (let i = 0; i < arr.length; i++) {
+    sorted.push(arr[i].toLowerCase());
+  };
+     sorted = sorted.filter((element,index) =>{
+      return sorted.indexOf(element) === index;
+    }) 
+    console.log(sorted)
+  }
+  
+  removeDuplicates( ['A', "a", 'B', 'A', 'C', 'B'])
+  
+  
