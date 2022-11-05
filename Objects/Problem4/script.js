5b7e6e849e8f17a091cd66abd40d8830c940a4df
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-11-05 10:36:15 -0700

    solution for object problem 4
    Object.hasOwn()  returns true if the specified object has the indicated property as its own property

--- Objects/Problem4/script.js
+++ Objects/Problem4/script.js
@@ -1,1 +1,22 @@
 //Create an object, then write a JavaScript function that checks whether an object contains the specified key.
+
+//create an object 
+
+let sampleObject = {}
+
+sampleObject.name = "test"
+sampleObject.company = "tribute"
+sampleObject.brand = "remarks"
+
+
+console.log(sampleObject)
+
+// create a function to check whether an object contains the specified key 
+
+function checkForKey(obj){
+    console.log(Object.hasOwn(obj, 'name'))
+}
+
+
+checkForKey(sampleObject)
+//expected output : True 
