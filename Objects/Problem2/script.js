b84b8453346cb304aa8ad5535e3d795ab6ee644b
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-11-05 09:47:25 -0700

    created array of people objects

--- Objects/Problem2/script.js
+++ Objects/Problem2/script.js
@@ -1,2 +1,12 @@
 // Create an array of people objects with first name, last name, and age, then write a JavaScript function that takes in an
 // array and console logs the first and last names of all the people.
+
+
+// create array of people objects
+let students = [ 
+    {firstName : "Sam" ,lastName : "Thomas", age: 10},
+    {firstName : "Lilly", lastName : "Thompson", age : 10}, 
+    {firstName : "Sally", lastName : "Hansen", age : 10 },
+    {firstName : "John" , lastName : "Doe" , age : 10 }
+]
+
