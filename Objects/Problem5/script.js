fde2b3f76741f6f2c0c597aee3d670a1948d610d
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-11-05 12:39:05 -0700

    solution object problem 5
    using the sort method to sort the title property in the array of objects by alpahabetical order

--- Objects/Problem5/script.js
+++ Objects/Problem5/script.js
@@ -1,9 +1,14 @@
 //Write a JavaScript function to sort the following array of objects by title value.
 /*
+// sort method changes the positions of the elements in the original array 
+//default sorts the elements with the smallest value first and largest value last
+// casts elements to strings and compares the strings to determine the order
 
 Hint: Read about the JavaScript sort method.
 
-Example array: [ 
+*/
+
+let array =  [ 
 
    { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
 
@@ -12,4 +17,19 @@ Example array: [
    { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
 
    ];
-*/
+
+function sortAlphabetically(arr, property){
+
+array.sort((a,b) => {
+  if (a.title > b.title) {
+    return 1 
+  } else {
+    return -1
+  }
+})
+
+console.log(array)
+  
+}
+
+sortAlphabetically(array)
