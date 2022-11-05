166eac857139215f7416489134fd727e5304247f
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-11-05 10:01:59 -0700

    solution objects problem 3
    find the length of an object using Object.keys().length

--- Objects/Problem3/script.js
+++ Objects/Problem3/script.js
@@ -1,1 +1,16 @@
 //Create an object with three items in it, then write a JavaScript function that accepts an object and tells you the length (tells you how many items are in it) of the object.
+
+let sampleObject = {}
+
+sampleObject.name = "test"
+sampleObject.company = "tribute"
+sampleObject.brand = "apple"
+
+function objectLength(obj){
+
+    objectLength = Object.keys(obj).length
+    console.log(objectLength)
+}
+
+
+objectLength(sampleObject)
